fc5e090282cf8de4f4cf639223d6e40ff4b82c15
Yana <yanamay98@gmail.com>
2019-12-04 16:45:21 +0300

    added background and walls

--- Tanks.cpp
+++ Tanks.cpp
@@ -1,16 +1,34 @@
 ﻿#include <iostream>
 #include <SFML/Graphics.hpp>
+#include "map.h"
 
 int main()
 {
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-	sf::RenderWindow window(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "Tanks");
+	sf::RenderWindow window(sf::VideoMode(640, 480, desktop.bitsPerPixel), "Tanks");
 
 	sf::Texture herotexture;
 	herotexture.loadFromFile("Textures/tank.png");
 	sf::Sprite herosprite;
 	herosprite.setTexture(herotexture);
-	herosprite.setPosition(50, 25);
+	herosprite.setPosition(64, 64);
+
+	sf::Texture bgtexture;
+	bgtexture.loadFromFile("Textures/bg.png");
+	sf::Sprite bgsprite;
+	bgsprite.setTexture(bgtexture);
+
+	sf::Texture walltexture;
+	walltexture.loadFromFile("Textures/wall.png");
+	sf::Sprite wallsprite;
+	wallsprite.setTexture(walltexture);
+
+	sf::Image map_image;//объект изображения для карты
+	map_image.loadFromFile("Textures/bg.png");//загружаем файл для карты
+	sf::Texture map;//текстура карты
+	map.loadFromImage(map_image);//заряжаем текстуру картинкой
+	sf::Sprite s_map;//создаём спрайт для карты
+	s_map.setTexture(map);//заливаем текстуру спрайтом 
 
 	while (window.isOpen())
 	{
@@ -22,6 +40,18 @@ int main()
 		}
 
 		window.clear();
+
+		for (int i = 0; i < HEIGHT_MAP; i++)
+			for (int j = 0; j < WIDTH_MAP; j++)
+			{
+				if (TileMap[i][j] == ' ') s_map.setTexture(bgtexture); //если
+				//встретили символ пробел, то рисуем 1-й квадратик
+				if ((TileMap[i][j] == '0')) s_map.setTexture(walltexture);//если
+				//встретили символ 0, то рисуем 3й квадратик
+				s_map.setPosition(j * 32, i * 32);//раскладываем квадратики в карту.
+				window.draw(s_map);//рисуем квадратики на экран
+			}
+
 		window.draw(herosprite);
 		window.display();
 	}
---
+++ map.h
@@ -0,0 +1,26 @@
+#ifndef _MAP_H_
+#define _MAP_H_
+
+#include <SFML\Graphics.hpp>
+const int HEIGHT_MAP = 15;
+const int WIDTH_MAP = 20;
+
+std::string TileMap[HEIGHT_MAP] = {
+	"00000000000000000000",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"0                  0",
+	"00000000000000000000",
+};
+
+#endif

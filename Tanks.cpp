03e3aebd3ac8009e22ded8e2f3fa0b00f59a2bcb
KanjoCaido <pavel.petruschka@ya.ru>
2019-12-04 22:38:27 +0300

    fixed Victoria's fixes

--- Tanks.cpp
+++ Tanks.cpp
@@ -1,111 +1,62 @@
 ﻿#include <iostream>
 #include <SFML/Graphics.hpp>
-using namespace sf;//включаем пространство имен sf, чтобы постоянно не писать sf::
+using namespace sf;
 
 int main()
 {
-	// Создаём окно с той же битовой глубиной пикселей, что и рабочий стол 
 	VideoMode desktop = sf::VideoMode::getDesktopMode();
 	RenderWindow window(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "Tanks");
+	window.setFramerateLimit(60);
 
+	Texture herotexture; 
+	herotexture.loadFromFile("Textures/tank.png"); 
 
-	Texture herotextureA, herotextureB, herotextureC, herotextureD; //cоздаем объект Texture(текстура)
-	herotextureA.loadFromFile("Textures/tank.png"); //передаем в него объект Image (изображения) 
-	/*herotextureB.loadFromFile("Textures/tankB.png");
-	herotextureC.loadFromFile("Textures/tankC.png");
-	herotextureD.loadFromFile("Textures/tankD.png");*/
-
-
-	Sprite herosprite; //создаем объект Sprite(спрайт) 
-	herosprite.setTexture(herotextureA); //передаём в него объект Texture (текстуры) 
-	herosprite.setPosition(250, 250); //задаем начальные координаты появления спрайта 
+	Sprite herosprite; 
+	herosprite.setTexture(herotexture); 
+	herosprite.setPosition(250, 250);  
 	herosprite.setOrigin(32, 32);
 
-	float CurrentFrame = 0;//хранит текущий кадр  
-	Clock clock;  //создаем переменную времени и одновременно запускаем часы! 
-
-
-	while (window.isOpen())  //Пока окно открыто 
+	while (window.isOpen())   
 	{
-
-		// дать время с последнего перезапуска часов, в данном случае время, прошедшее с               
-		//предыдущей итерации и вызова restart();   
-		float time = clock.getElapsedTime().asMicroseconds();    
-		clock.restart(); //перезапуск часов   
-		time = time / 800; //скорость игры   
-
-
-		Event event; //Переменная для события
-		while (window.pollEvent(event)) //Опрос событий 
+		Event event; 
+		while (window.pollEvent(event)) 
 		{
 			if (event.type == Event::Closed)
-				window.close(); //Закрываем окно, если событие “Closed” 
+				window.close(); 
 		}
-
-		
-		//координата Y, на которой герой изображен идущим влево равна 96   
-		if (((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))))
+  
+		if (Keyboard::isKeyPressed(Keyboard::Left))
 		{    
-			//если нажата клавиша стрелка влево или англ буква А   
-			CurrentFrame += 0.000005*time; 
-
-			//служит для прохождения по "кадрам". переменная доходит до трех, суммируя произведение 
-			//времени и скорости. Изменив 0.005, можно изменить скорость анимации    
-			if (CurrentFrame > 3) CurrentFrame -= 3; //если пришли к третьему кадру - 
-													 //откатываемся назад. 
-			
-			herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
-			herosprite.move(-0.1 * time, 0); //происходит движение персонажа влево
+			herosprite.setTexture(herotexture);
+			herosprite.move(-2.5, 0); 
 			herosprite.setRotation(270);
-			
-			
 		} 
-
-		//координата Y, на которой герой изображен идущем вправо равна 96+96=192   
-		if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D))))
+   
+		if (Keyboard::isKeyPressed(Keyboard::Right)) 
 		{    
-			CurrentFrame += 0.000005 * time;     
-			if (CurrentFrame > 3) CurrentFrame -= 3;
-
-			//herosprite.setTexture(herotextureB);
-			herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
-			herosprite.move(0.1 * time, 0);
+			herosprite.setTexture(herotexture);
+			herosprite.move(2.5, 0); 
 			herosprite.setRotation(90);
 			
 		} 
 
-		//координата Y на которой герой изображен идущим вверх равна 288   
-		if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W))))
+		if (Keyboard::isKeyPressed(Keyboard::Up) && !Keyboard::isKeyPressed(Keyboard::Left) && !Keyboard::isKeyPressed(Keyboard::Right))
 		{    
-			CurrentFrame += 0.000005 * time;     
-			if (CurrentFrame > 3) CurrentFrame -= 3;
-
-			//herosprite.setTexture(herotextureA);
-			herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
-			herosprite.move(0, -0.1 * time);
-			herosprite.setRotation(0);
-			
+			herosprite.setTexture(herotexture);
+			herosprite.move(0, -2.5);
+			herosprite.setRotation(0);	
 		} 
 
-		  
-		if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S))))
+		if (Keyboard::isKeyPressed(Keyboard::Down) && !Keyboard::isKeyPressed(Keyboard::Left) && !Keyboard::isKeyPressed(Keyboard::Right))
 		{   
-			CurrentFrame += 0.000005 * time;     
-			if (CurrentFrame > 3) CurrentFrame -= 3;
-
-			//herosprite.setTexture(herotextureC);
-			 
-			herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
-			herosprite.move(0, 0.1 * time);
+			herosprite.setTexture(herotexture);
+			herosprite.move(0, 2.5); 
 			herosprite.setRotation(180);
-			
 		}   
 
-
-		window.clear(); //Очищаем экран 
-		window.draw(herosprite); //Рисуем herosprite
-		window.display(); //Отображаем herosprite на экран 
+		window.clear(); 
+		window.draw(herosprite); 
+		window.display(); 
 	}
-
 	return 0;
 }

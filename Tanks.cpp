a4ec50f061d427d5ed22d193792af85e285c3240
Vika <schemiakina2011@yandex.ru>
2019-12-04 15:47:09 +0300

    added sfml, added tank

--- Tanks.cpp
+++ Tanks.cpp
@@ -1,29 +1,32 @@
 ﻿#include <iostream>
 #include <SFML/Graphics.hpp>
+using namespace sf;//включаем пространство имен sf, чтобы постоянно не писать sf::
 
 int main()
 {
-	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-	sf::RenderWindow window(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "Tanks");
+	// Создаём окно с той же битовой глубиной пикселей, что и рабочий стол 
+	VideoMode desktop = sf::VideoMode::getDesktopMode();
+	RenderWindow window(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "Tanks");
 
-	sf::Texture herotexture;
-	herotexture.loadFromFile("Textures/tank.png");
-	sf::Sprite herosprite;
-	herosprite.setTexture(herotexture);
-	herosprite.setPosition(50, 25);
+	Texture herotexture; //cоздаем объект Texture(текстура)
+	herotexture.loadFromFile("Textures/tank.png"); //передаем в него объект Image (изображения) 
 
-	while (window.isOpen())
+	Sprite herosprite; //создаем объект Sprite(спрайт) 
+	herosprite.setTexture(herotexture); //передаём в него объект Texture (текстуры) 
+	herosprite.setPosition(50, 25); //задаем начальные координаты появления спрайта 
+
+	while (window.isOpen())  //Пока окно открыто 
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		Event event; //Переменная для события
+		while (window.pollEvent(event)) //Опрос событий 
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			if (event.type == Event::Closed)
+				window.close(); //Закрываем окно, если событие “Closed” 
 		}
 
-		window.clear();
-		window.draw(herosprite);
-		window.display();
+		window.clear(); //Очищаем экран 
+		window.draw(herosprite); //Рисуем herosprite
+		window.display(); //Отображаем herosprite на экран 
 	}
 
 	return 0;

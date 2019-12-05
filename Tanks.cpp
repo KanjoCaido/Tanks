ef44afc7c787b9d4d88aac972c185cfb6cacbc18
Yana <yanamay98@gmail.com>
2019-12-05 18:53:33 +0300

    added object to protect

--- Tanks.cpp
+++ Tanks.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-	sf::RenderWindow window(sf::VideoMode(640, 480, desktop.bitsPerPixel), "Tanks");
+	sf::RenderWindow window(sf::VideoMode(608, 480, desktop.bitsPerPixel), "Tanks");
 
 	sf::Texture herotexture;
 	herotexture.loadFromFile("Textures/tank.png");
@@ -13,10 +13,10 @@ int main()
 	herosprite.setTexture(herotexture);
 	herosprite.setPosition(64, 64);
 
-/*	sf::Texture kokotexture;
-	kokotexture.loadFromFile("Textures/koko.png");
-	sf::Sprite kokosprite;
-	kokosprite.setTexture(kokotexture); */
+	/*	sf::Texture kokotexture;
+		kokotexture.loadFromFile("Textures/koko.png");
+		sf::Sprite kokosprite;
+		kokosprite.setTexture(kokotexture); */
 
 	sf::Texture bgtexture;
 	bgtexture.loadFromFile("Textures/bg.png");
@@ -28,12 +28,17 @@ int main()
 	sf::Sprite wallsprite;
 	wallsprite.setTexture(walltexture);
 
+	sf::Texture kokotexture;
+	kokotexture.loadFromFile("Textures/koko.png");
+	sf::Sprite kokosprite;
+	kokosprite.setTexture(kokotexture);
+
 	sf::Image map_image;
 	map_image.loadFromFile("Textures/bg.png");
 	sf::Texture map;
 	map.loadFromImage(map_image);
 	sf::Sprite s_map;
-	s_map.setTexture(map); 
+	s_map.setTexture(map);
 
 	while (window.isOpen())
 	{
@@ -49,10 +54,10 @@ int main()
 		for (int i = 0; i < HEIGHT_MAP; i++)
 			for (int j = 0; j < WIDTH_MAP; j++)
 			{
-				if (TileMap[i][j] == ' ') s_map.setTexture(bgtexture); 
+				if (TileMap[i][j] == ' ') s_map.setTexture(bgtexture);
 				if (TileMap[i][j] == '0') s_map.setTexture(walltexture);
-//				if (TileMap[i][j] == 'l') s_map.setTextureRect(sf::IntRect(0,0,16,32));
-//				if (TileMap[i][j] == 'p') s_map.setTextureRect(sf::IntRect(16,0,32,32));
+				if (TileMap[i][j] == 'c') s_map.setTexture(kokotexture);
+				
 				s_map.setPosition(j * 32, i * 32);
 				window.draw(s_map);
 			}
